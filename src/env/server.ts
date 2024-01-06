514235c54fb0406fe3732f1cfd0a3cfdb7ba5381
Visibyte <nathanglass26@gmail.com>
2024-01-06 11:12:51 +0000

    auth secret

--- src/env/server.ts
+++ src/env/server.ts
@@ -6,10 +6,12 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
+    AUTH_SECRET: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
   },
 });
